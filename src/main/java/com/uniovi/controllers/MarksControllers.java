ed8ba4c0a5675f51e05bbdf21fe12e8a90570648
adanvetusta <adanvetusta@gmail.com>
2019-02-14 20:42:24 +0100

    SDI-SpringBoot-2-Thymeleaft-fragmentos

--- src/main/java/com/uniovi/controllers/MarksControllers.java
+++ src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -61,4 +61,10 @@ public class MarksControllers {
 		return "redirect:/mark/details/" + id;
 	}
 
+	@RequestMapping("/mark/list/update")
+	public String updateList(Model model) {
+		model.addAttribute("markList", marksService.getMarks());
+		return "mark/list :: tableMarks";
+	}
+
 }
