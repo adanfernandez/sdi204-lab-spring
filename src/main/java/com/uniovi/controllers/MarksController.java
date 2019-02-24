6279b54b562fd50b1f9fa05de9208ccc12fea8f9
adanvetusta <adanvetusta@gmail.com>
2019-02-24 13:55:48 +0100

    SDI-SpringBoot-Sesión-4.4-Consulta

--- src/main/java/com/uniovi/controllers/MarksController.java
+++ src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,5 +1,7 @@
 package com.uniovi.controllers;
 
+import java.security.Principal;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.*;
@@ -23,14 +25,18 @@ public class MarksController {
 	private HttpSession httpSession;
 
 	@RequestMapping("/mark/list/update")
-	public String updateList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String updateList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list :: tableMarks";
 	}
 
 	@RequestMapping("/mark/list")
-	public String getList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String getList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list";
 	}
 
