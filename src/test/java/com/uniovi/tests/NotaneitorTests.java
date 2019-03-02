a07584ff6dfbf2906ba153928068c4fb5bd80502
adanvetusta <adanvetusta@gmail.com>
2019-03-02 11:24:47 +0100

    SDI-Selenium-5.1-ClasePrincipal

---
+++ src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -0,0 +1,70 @@
+package com.uniovi.tests;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+//Ordenamos las pruebas por el nombre del método
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class NotaneitorTests {
+
+	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
+	// automáticas)):
+	// static String PathFirefox64 = "C:\\Program Files\\Mozilla
+	// Firefox\\firefox.exe";
+	// static String Geckdriver022 = "C:\\Path\\geckodriver024win64.exe";
+	// En MACOSX (Debe ser la versión 65.0.1 y desactivar las actualizacioens
+	// automáticas):
+
+	static String PathFirefox65 = "C:\\Users\\adanv\\Desktop\\FirefoxPortable\\App\\Firefox64\\firefox.exe";
+	static String Geckdriver024 = "C:\\Users\\adanv\\Desktop\\EII\\SDI\\Laboratorio\\5\\PL-SDI-Sesión5-material\\pl5\\geckodriver022win64.exe";
+	// Común a Windows y a MACOSX
+	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
+	static String URL = "http://localhost:8090";
+
+	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+
+	// Antes de cada prueba se navega al URL home de la aplicaciónn
+	@Before
+	public void setUp() {
+		driver.navigate().to(URL);
+	}
+
+	// Después de cada prueba se borran las cookies del navegador
+	@After
+	public void tearDown() {
+		driver.manage().deleteAllCookies();
+	}
+
+	// Antes de la primera prueba
+	@BeforeClass
+	static public void begin() {
+	}
+
+	// Al finalizar la última prueba
+	@AfterClass
+	static public void end() {
+		// Cerramos el navegador al finalizar las pruebas
+		driver.quit();
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Todo OK, Jose Luis");
+	}
+	
+}
