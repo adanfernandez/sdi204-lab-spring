1cd624d1339e5e1498505fb50689f66f70b4ced6
adanvetusta <adanvetusta@gmail.com>
2019-03-02 19:26:56 +0100

    SDI-Selenium-5.5-CasosUso-VistaRegister

--- src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -1,17 +1,41 @@
 package com.uniovi.tests;
 
 import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.*;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.uniovi.tests.pageobjects.PO_HomeView;
 import com.uniovi.tests.pageobjects.PO_Properties;
+import com.uniovi.tests.pageobjects.PO_RegisterView;
+import com.uniovi.tests.pageobjects.PO_View;
+import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
+import com.uniovi.services.RolesService;
+import com.uniovi.services.UsersService;
+import com.uniovi.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class NotaneitorTests {
+
+	@Autowired
+	private UsersService usersService;
+	@Autowired
+	private RolesService rolesService;
+	@Autowired
+	private UsersRepository usersRepository;
 
 	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
 	// automáticas)):
@@ -37,6 +61,7 @@ public class NotaneitorTests {
 	// Antes de cada prueba se navega al URL home de la aplicaciónn
 	@Before
 	public void setUp() {
+		initdb();
 		driver.navigate().to(URL);
 	}
 
@@ -57,6 +82,55 @@ public class NotaneitorTests {
 		// Cerramos el navegador al finalizar las pruebas
 		driver.quit();
 	}
+
+	public void initdb() {
+		//Borramos todas las entidades.
+		usersRepository.deleteAll();
+		
+		//Ahora las volvemos a crear
+		User user1 = new User("99999990A", "Pedro", "Díaz");
+		user1.setPassword("123456");
+		user1.setRole(rolesService.getRoles()[0]);
+		User user2 = new User("99999991B", "Lucas", "Núñez");
+		user2.setPassword("123456");
+		user2.setRole(rolesService.getRoles()[0]);
+		User user3 = new User("99999992C", "María", "Rodríguez");
+		user3.setPassword("123456");
+		user3.setRole(rolesService.getRoles()[0]);
+		User user4 = new User("99999993D", "Marta", "Almonte");
+		user4.setPassword("123456");
+		user4.setRole(rolesService.getRoles()[1]);
+		User user5 = new User("99999977E", "Pelayo", "Valdes");
+		user5.setPassword("123456");
+		user5.setRole(rolesService.getRoles()[1]);
+		User user6 = new User("99999988F", "Edward", "Núñez");
+		user6.setPassword("123456");
+		user6.setRole(rolesService.getRoles()[2]);
+		Set<Mark> user1Marks = new HashSet<Mark>();
+		for(int i = 1; i <5; i++)
+		 user1Marks.add(new Mark("Nota A"+String.valueOf(i), i*1.0, user1));
+		user1.setMarks(user1Marks);
+		Set<Mark> user2Marks = new HashSet<Mark>();
+		for(int i = 1; i <5; i++)
+		 user2Marks.add(new Mark("Nota A"+String.valueOf(i), i*1.0, user2));
+		user2.setMarks(user2Marks);
+		Set<Mark> user3Marks = new HashSet<Mark>();
+		for(int i = 1; i <8; i++)
+		 user3Marks.add(new Mark("Nota A"+String.valueOf(i), i*1.0, user3));
+		user3.setMarks(user3Marks);
+		Set<Mark> user4Marks = new HashSet<Mark>();
+		for(int i = 1; i <15; i++)
+		 user4Marks.add(new Mark("Nota A"+String.valueOf(i), i*1.0, user4));
+		//OJO que la prueba 15 que trata de borrar esta nota no siempre la encuentra en la última página
+		user4Marks.add(new Mark("Nota Nueva 1", 9.0, user4));
+		user4.setMarks(user4Marks);
+		usersService.addUser(user1);
+		usersService.addUser(user2);
+		usersService.addUser(user3);
+		usersService.addUser(user4);
+		usersService.addUser(user5);
+		usersService.addUser(user6);
+		}
 
 	// PR01. Acceder a la página principal /
 	@Test
@@ -86,4 +160,33 @@ public class NotaneitorTests {
 		// SeleniumUtils.esperarSegundos(driver, 2);
 	}
 
+	// PR05. Prueba del formulario de registro. registro con datos correctos
+	@Test
+	public void PR05() {
+		// Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		// Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "77777778A", "Josefo", "Perez", "77777", "77777");
+		// Comprobamos que entramos en la sección privada
+		PO_View.checkElement(driver, "text", "Notas del usuario");
+	}
+
+	// PR06. Prueba del formulario de registro. DNI repetido en la BD, Nombre corto,
+	// .... pagination pagination-centered, Error.signup.dni.length
+	@Test
+	public void PR06() {
+		// Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		// Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "99999990A", "Josefo", "Perez", "77777", "77777");
+		PO_View.getP();
+		// COmprobamos el error de DNI repetido.
+		PO_RegisterView.checkKey(driver, "Error.signup.dni.duplicate", PO_Properties.getSPANISH());
+		// Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "99999990B", "Jose", "Perez", "77777", "77777");
+		// COmprobamos el error de Nombre corto .
+		PO_RegisterView.checkKey(driver, "Error.signup.name.length", PO_Properties.getSPANISH());
+		// Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "99999990B", "Josefo", "Per", "77777", "77777");
+	}
 }
